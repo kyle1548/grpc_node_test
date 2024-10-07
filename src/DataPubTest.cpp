481ce858af26a7c06ab58039733f90816e13d9b7
kyle1548 <kyle9296@gmail.com>
2024-10-08 07:14:07 +0800

    first

---
+++ src/DataPubTest.cpp
@@ -0,0 +1,127 @@
+#include "NodeHandler.h"
+#include "Motor.pb.h"
+#include "Power.pb.h"
+#include <sys/time.h>
+
+int main() {
+    // Publisher //
+    core::NodeHandler nh;
+    core::Rate rate(1000);
+    core::Publisher<motor_msg::MotorCmdStamped> &pub_motor_cmd = nh.advertise<motor_msg::MotorCmdStamped>("Motor/command");
+    core::Publisher<motor_msg::MotorStateStamped> &pub_motor_state = nh.advertise<motor_msg::MotorStateStamped>("Motor/state");
+    core::Publisher<power_msg::PowerCmdStamped> &pub_power_cmd = nh.advertise<power_msg::PowerCmdStamped>("Power/command");
+    core::Publisher<power_msg::PowerStateStamped> &pub_power_state = nh.advertise<power_msg::PowerStateStamped>("Power/state");
+    
+    // Motor & Power msg //
+    motor_msg::MotorCmdStamped   motor_cmd;
+    motor_msg::MotorStateStamped motor_state;
+    power_msg::PowerCmdStamped   power_cmd;
+    power_msg::PowerStateStamped power_state;
+    std::vector< motor_msg::MotorCmd*> modules_cmd = {
+        motor_cmd.mutable_module_a(),
+        motor_cmd.mutable_module_b(),
+        motor_cmd.mutable_module_c(),
+        motor_cmd.mutable_module_d()
+    };
+    std::vector< motor_msg::MotorState*> modules_state = {
+        motor_state.mutable_module_a(),
+        motor_state.mutable_module_b(),
+        motor_state.mutable_module_c(),
+        motor_state.mutable_module_d()
+    };
+
+    // Initial values //
+    int seq = 0;
+    double theta = 0.0;
+    double beta = 0.0;
+    double ki = 50.5;
+    double kp = 10.6;
+    double kd = 40.7;
+    double current_r = 0.7;
+    double current_l = 0.7;
+    double v = 0.0;
+    double i = 0.0;
+
+    // While forever //
+    timeval currentTime;
+    while (1)
+    {
+        const auto start = std::chrono::steady_clock::now();
+        // Header //
+        gettimeofday(&currentTime, nullptr);
+        motor_cmd.mutable_header()->set_seq(seq);
+        motor_cmd.mutable_header()->mutable_stamp()->set_sec(currentTime.tv_sec);
+        motor_cmd.mutable_header()->mutable_stamp()->set_usec(currentTime.tv_usec);
+        motor_state.mutable_header()->set_seq(seq);
+        motor_state.mutable_header()->mutable_stamp()->set_sec(currentTime.tv_sec);
+        motor_state.mutable_header()->mutable_stamp()->set_usec(currentTime.tv_usec);
+        power_cmd.mutable_header()->set_seq(seq);
+        power_cmd.mutable_header()->mutable_stamp()->set_sec(currentTime.tv_sec);
+        power_cmd.mutable_header()->mutable_stamp()->set_usec(currentTime.tv_usec);
+        power_state.mutable_header()->set_seq(seq);
+        power_state.mutable_header()->mutable_stamp()->set_sec(currentTime.tv_sec);
+        power_state.mutable_header()->mutable_stamp()->set_usec(currentTime.tv_usec);
+        seq ++;
+
+        // Motor modules //
+        for (auto& module : modules_cmd) {
+            module->set_theta(theta);
+            module->set_beta(beta);
+            module->set_ki(ki);
+            module->set_kp(kp);
+            module->set_kd(kd);
+        }
+        for (auto& module : modules_state) {
+            module->set_theta(theta);
+            module->set_beta(beta);
+            module->set_current_r(current_r);
+            module->set_current_l(current_l);
+        }
+        theta += 0.1;
+        beta += 0.1;
+        current_r += 0.1;
+        current_l += 0.1;
+
+        // Power //
+        bool digital = seq % 2 == 0;
+        bool power = seq % 2 == 0;
+        int  motor_mode = seq % 5;
+        power_cmd.set_digital(digital);
+        power_cmd.set_power(power);
+        power_cmd.set_motor_mode((power_msg::MOTORMODE)motor_mode);
+        power_state.set_digital(digital);
+        power_state.set_power(power);
+        power_state.set_motor_mode((power_msg::MOTORMODE)motor_mode);
+
+        // Power V & I //
+        power_state.set_v_0(v);
+        power_state.set_i_0(i);
+        power_state.set_v_1(v);
+        power_state.set_i_1(i);
+        power_state.set_v_2(v);
+        power_state.set_i_2(i);
+        power_state.set_v_3(v);
+        power_state.set_i_3(i);
+        /*...*/
+        power_state.set_v_11(v);
+        power_state.set_i_11(v);
+        v += 0.5;
+        i += 0.5;
+
+        // Publish //
+        std::cout << "MotorCmd   message length\t" << motor_cmd.ByteSizeLong() << "\n";
+        std::cout << "MotorState message length\t" << motor_state.ByteSizeLong() << "\n";
+        std::cout << "PowerCmd   message length\t" << power_cmd.ByteSizeLong() << "\n";
+        std::cout << "PowerState message length\t" << power_state.ByteSizeLong() << "\n";
+        pub_motor_cmd.publish(motor_cmd);
+        pub_motor_state.publish(motor_state);
+        pub_power_cmd.publish(power_cmd);
+        pub_power_state.publish(power_state);
+
+        // Using time //
+        const auto diff = std::chrono::steady_clock::now() - start;
+        std::cout << std::chrono::duration<double>(diff).count() << " seconds\n\n";
+	    std::cout << rate.sleep() << "\n";
+    }
+    return 0;
+}
---
+++ src/DataSubTest.cpp
@@ -0,0 +1,90 @@
+#include "NodeHandler.h"
+#include "Motor.pb.h"
+#include "Power.pb.h"
+
+std::mutex mutex_;
+
+void motor_cmd_cb(motor_msg::MotorCmdStamped msg) {
+    mutex_.lock();
+    std::cout << "=====" << "MotorCmd" << "=====" << "\n";
+    std::cout << "Header  : " << msg.header().seq() << "\n";
+    std::cout << "Module A: " << "\n";
+    std::cout << "  Theta: " << msg.module_a().theta() << "\n";
+    std::cout << "  Beta : " << msg.module_a().beta() << "\n";
+    std::cout << "  kp   : " << msg.module_a().kp() << "\n";
+    std::cout << "  ki   : " << msg.module_a().ki() << "\n";
+    std::cout << "  kd   : " << msg.module_a().kd() << "\n";
+    mutex_.unlock();
+}
+
+void motor_state_cb(motor_msg::MotorStateStamped msg) {
+    mutex_.lock();
+    std::cout << "=====" << "MotorState" << "=====" << "\n";
+    std::cout << "Header  : " << msg.header().seq() << "\n";
+    std::cout << "Module A: " << "\n";
+    std::cout << "  Theta    : " << msg.module_a().theta() << "\n";
+    std::cout << "  Beta     : " << msg.module_a().beta() << "\n";
+    std::cout << "  current_r: " << msg.module_a().current_r() << "\n";
+    std::cout << "  current_l: " << msg.module_a().current_l() << "\n";
+    mutex_.unlock();
+}
+
+void power_cmd_cb(power_msg::PowerCmdStamped msg) {
+    mutex_.lock();
+    std::cout << "=====" << "PowerCmd" << "=====" << "\n";
+    std::cout << "Header : " << msg.header().seq() << "\n";
+    std::cout << "Digital: " << msg.digital() << "\n";
+    std::cout << "Power  : " << msg.power() << "\n";
+    std::cout << "Mode   : " << msg.motor_mode() << "\n";
+    mutex_.unlock();
+}
+
+void power_state_cb(power_msg::PowerStateStamped msg) {
+    mutex_.lock();
+    std::cout << "=====" << "PowerState" << "=====" << "\n";
+    std::cout << "Header : " << msg.header().seq() << "\n";
+    std::cout << "Digital: " << msg.digital() << "\n";
+    std::cout << "V 0    : " << msg.v_0() << "\n";
+    std::cout << "I 0    : " << msg.i_0() << "\n";
+    std::cout << "V 1    : " << msg.v_1() << "\n";
+    std::cout << "I 1    : " << msg.i_1() << "\n";    
+    std::cout << "V 2    : " << msg.v_2() << "\n";
+    std::cout << "I 2    : " << msg.i_2() << "\n";    
+    std::cout << "V 3    : " << msg.v_3() << "\n";
+    std::cout << "I 3    : " << msg.i_3() << "\n";    
+    std::cout << "V 4    : " << msg.v_4() << "\n";
+    std::cout << "I 4    : " << msg.i_4() << "\n";    
+    std::cout << "V 5    : " << msg.v_5() << "\n";
+    std::cout << "I 5    : " << msg.i_5() << "\n";    
+    std::cout << "V 6    : " << msg.v_6() << "\n";
+    std::cout << "I 6    : " << msg.i_6() << "\n";    
+    std::cout << "V 7    : " << msg.v_7() << "\n";
+    std::cout << "I 7    : " << msg.i_7() << "\n";
+    std::cout << "V 8    : " << msg.v_8() << "\n";
+    std::cout << "I 8    : " << msg.i_8() << "\n";
+    std::cout << "V 9    : " << msg.v_9() << "\n";
+    std::cout << "I 9    : " << msg.i_9() << "\n";
+    std::cout << "V 10   : " << msg.v_10() << "\n";
+    std::cout << "I 10   : " << msg.i_10() << "\n";
+    std::cout << "V 11   : " << msg.v_11() << "\n";
+    std::cout << "I 11   : " << msg.i_11() << "\n";
+    mutex_.unlock();
+}
+
+int main() {
+    // Subscriber //
+    core::NodeHandler nh;
+    core::Rate rate(1000);
+    core::Subscriber<motor_msg::MotorCmdStamped> &sub_motor_cmd = nh.subscribe<motor_msg::MotorCmdStamped>("Motor/command", 1000, motor_cmd_cb);
+    core::Subscriber<motor_msg::MotorStateStamped> &sub_motor_state = nh.subscribe<motor_msg::MotorStateStamped>("Motor/state", 1000, motor_state_cb);
+    core::Subscriber<power_msg::PowerCmdStamped> &sub_power_cmd = nh.subscribe<power_msg::PowerCmdStamped>("Power/command", 1000, power_cmd_cb);
+    core::Subscriber<power_msg::PowerStateStamped> &sub_power_state = nh.subscribe<power_msg::PowerStateStamped>("Power/state", 1000, power_state_cb);
+    
+    // While forever //
+    while (1)
+    {
+        core::spinOnce();
+        std::cout << "sleep: " << rate.sleep() << "\n";
+    }
+    return 0;
+}
